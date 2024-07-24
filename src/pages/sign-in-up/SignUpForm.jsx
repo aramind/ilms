620551f9c8579a9cced04bcdfa6809d3616228a3
aramind <88814622+aramind@users.noreply.github.com>
2024-07-24 22:07:38 +0800

    feat: installing yup

---
+++ src/pages/sign-in-up/SignUpForm.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import FormWrapper from "../../wrappers/FormWrapper";
+import { useForm } from "react-hook-form";
+
+const SignUpForm = () => {
+  //  const {control, handleSubmit} = useForm({
+  //     mode: "onTouched", resolver: yupResolver
+  //  })
+  return (
+    <>
+      <FormWrapper></FormWrapper>
+    </>
+  );
+};
+
+export default SignUpForm;
