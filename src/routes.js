56e9f1955e88699bb478dea58da8c174e6b352ac
aramind <88814622+aramind@users.noreply.github.com>
2024-07-26 13:15:35 +0800

    feat: adding route for Dashboard

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,17 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
 import SignUpInMain from "./pages/sign-in-up/SignUpInMain";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
     children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
       {
         path: "/signup",
         element: <SignUpInMain />,
