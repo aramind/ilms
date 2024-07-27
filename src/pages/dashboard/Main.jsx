6100b7191ee2b7dde2aa1e0135d0484acc8cc140
aramind <88814622+aramind@users.noreply.github.com>
2024-07-28 01:45:16 +0800

    feat: wrapping the content of my courses on dashboard

--- src/pages/dashboard/Main.jsx
+++ src/pages/dashboard/Main.jsx
@@ -11,16 +11,35 @@ const Main = () => {
       <WhiteTypography>Hi Robin! Good Luck with your studies!</WhiteTypography>
       <Stack>
         <WhiteTypography variant="h6">My Courses</WhiteTypography>
+
         <Stack
           direction="row"
           justifyContent="flex-start"
-          spacing={2}
-          sx={{
-            borderRadius: "0.5rem",
-            p: 2,
-            // bgcolor: (theme) => theme.palette.black.main,
-          }}
+          // spacing={2}
+          gap={2}
+          width={{ xs: "100vw", md: "100%" }}
+          flexWrap="wrap"
         >
+          <CourseCard
+            title="Mathematics"
+            description=" Review course for Electronics in the ECE Licensure Examination."
+            progress="30"
+          />
+          <CourseCard
+            title="Electronics"
+            description=" Review course for Electronics in the ECE Licensure Examination."
+            progress="30"
+          />
+          <CourseCard
+            title="GEAS"
+            description=" Review course for Electronics in the ECE Licensure Examination."
+            progress="30"
+          />
+          <CourseCard
+            title="Electronic Systems and Technologies"
+            description=" Review course for Electronics in the ECE Licensure Examination.Review course for Electronics in the ECE Licensure Examination.Review course for Electronics in the ECE Licensure Examination."
+            progress="30"
+          />
           <CourseCard
             title="Mathematics"
             description=" Review course for Electronics in the ECE Licensure Examination."
