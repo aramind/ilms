a5600e386688218cdcdee35d5ef7969ccbd68429
aramind <88814622+aramind@users.noreply.github.com>
2024-07-29 02:14:53 +0800

    refactor: changing text contents of unavailable page

--- src/pages/unavailable/UnavailablePage.jsx
+++ src/pages/unavailable/UnavailablePage.jsx
@@ -1,16 +1,35 @@
 import React from "react";
 import WhiteTypography from "../../components/WhiteTypography";
 import MainLayoutWrapper from "../../wrappers/MainLayoutWrapper";
-import { Box } from "@mui/material";
+import { Box, colors, Link, Stack } from "@mui/material";
 
 const UnavailablePage = () => {
   return (
     <MainLayoutWrapper>
-      <Box width={1}>
-        <WhiteTypography sx={{ fontSize: "1.5rem" }}>
-          Sorry, this feature is not available yet.
+      <Stack alignItems="center" width={1} mt={5}>
+        <WhiteTypography sx={{ textAlign: "center" }}>
+          Sorry, 😞 feature not yet implemented. Dev run out of
+          motivation.💸💸💸
         </WhiteTypography>
-      </Box>
+        <WhiteTypography>
+          Help motivate by donating some funds. 🙏🏻🙏🏻🙏🏻
+        </WhiteTypography>
+        <Stack direction="row" spacing={1} mt={4}>
+          <WhiteTypography>Click here to</WhiteTypography>
+          <WhiteTypography>
+            {" "}
+            <Link
+              href="https://www.linkedin.com/in/robin-mon-miranda/"
+              target="_blank"
+              rel="noreferrer"
+              sx={{ color: (theme) => theme.palette.secondary.main }}
+            >
+              Donate
+            </Link>
+            &nbsp; &nbsp; &nbsp;😊
+          </WhiteTypography>
+        </Stack>
+      </Stack>
     </MainLayoutWrapper>
   );
 };
