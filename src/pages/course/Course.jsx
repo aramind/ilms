4bdbfeba437bc252eab456626af54b795e3a045a
aramind <88814622+aramind@users.noreply.github.com>
2024-07-31 02:08:21 +0800

    feat: using accordion to display topics in course page

--- src/pages/course/Course.jsx
+++ src/pages/course/Course.jsx
@@ -1,11 +1,19 @@
 import React, { useMemo } from "react";
 import MainLayoutWrapper from "../../wrappers/MainLayoutWrapper";
 import { useParams } from "react-router-dom";
-import { Box, Stack } from "@mui/material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Stack,
+  Typography,
+} from "@mui/material";
 import WhiteTypography from "../../components/WhiteTypography";
 import ProgressIndicator from "../../components/card/ProgressIndicator";
 import { db } from "../../configs/db";
 import { getTopicProgress, getTopicsInCourse } from "../../configs/API";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const Course = () => {
   const { courseId } = useParams();
@@ -25,6 +33,7 @@ const Course = () => {
     });
   }, [courseId]);
 
+  console.log(updatedTopics);
   return (
     <MainLayoutWrapper>
       <Stack spacing={2} alignItems={{ xs: "center", md: "flex-start" }}>
@@ -39,6 +48,105 @@ const Course = () => {
             </Box>
           ))}
         </Stack>
+        {/* {updatedTopics?.map((topic, index) => (
+          <Stack
+            key={index}
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            px={2}
+            py={1}
+            width={1}
+            borderRadius="1rem"
+            bgcolor={(theme) => theme.palette.black.dark}
+            //   onClick={() => alert("Clicked")}
+            sx={{
+              // Set cursor to pointer
+              "&:hover": {
+                //   backgroundColor: "#ffffff", // Optional: change background color on hover
+                outline: "2px solid",
+                outlineColor: (theme) => theme.palette.primary.darkest,
+              },
+            }}
+          >
+            <Stack width={1} direction="row" justifyContent="space-between">
+              <WhiteTypography variant="h5">
+                Lecture {index + 1} : {topic.title}
+              </WhiteTypography>
+              <Typography variant="h5" color="primary" fontWeight="bold">
+                {topic?.progress}%
+              </Typography>
+            </Stack>
+            <Box width={1}>
+              <ProgressIndicator value={topic?.progress || 0} height="8px" />
+            </Box>
+            <Stack width={1}>
+              {topic?.topicTasks?.map((task, j) => (
+                <WhiteTypography key={index}>
+                  {index + 1}.{j + 1}. {task.instruction}
+                </WhiteTypography>
+              ))}
+            </Stack>
+          </Stack>
+        ))} */}
+        {updatedTopics?.map((topic, index) => (
+          <Accordion
+            key={index}
+            sx={{
+              width: "100%",
+              borderRadius: "1rem",
+              bgcolor: (theme) => theme.palette.black.dark,
+              mb: 2,
+              "&:before": {
+                display: "none",
+              },
+            }}
+          >
+            <AccordionSummary
+              expandIcon={
+                <ExpandMoreIcon
+                  sx={{
+                    color: (theme) => theme.palette.primary.main,
+                    fontSize: "2rem",
+                  }}
+                />
+              }
+              aria-controls={`panel${index}-content`}
+              id={`panel${index}-header`}
+            >
+              <Stack width={1} pr={2}>
+                <Stack width={1} direction="row" justifyContent="space-between">
+                  <WhiteTypography variant="h5">
+                    Lecture {index + 1} : {topic.title}
+                  </WhiteTypography>
+                  <Typography variant="h5" color="primary" fontWeight="bold">
+                    {topic?.progress}%
+                  </Typography>
+                </Stack>
+                <Box width={1}>
+                  <ProgressIndicator
+                    value={topic?.progress || 0}
+                    height="8px"
+                  />
+                </Box>
+              </Stack>
+            </AccordionSummary>
+            <AccordionDetails
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                gap: 1,
+              }}
+            >
+              <Stack width={1}>
+                {topic?.topicTasks?.map((task, j) => (
+                  <WhiteTypography key={j}>
+                    {index + 1}.{j + 1}. {task.instruction}
+                  </WhiteTypography>
+                ))}
+              </Stack>
+            </AccordionDetails>
+          </Accordion>
+        ))}
       </Stack>
     </MainLayoutWrapper>
   );
