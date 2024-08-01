2dcd8a18f02280c8ef0b6a20d2b6a4d18ebd0404
aramind <88814622+aramind@users.noreply.github.com>
2024-08-01 17:14:31 +0800

    feat: using page header component

--- src/pages/dashboard/Main.jsx
+++ src/pages/dashboard/Main.jsx
@@ -1,6 +1,5 @@
 import { Stack } from "@mui/material";
 import React from "react";
-import WhiteTypography from "../../components/WhiteTypography";
 import CourseCard from "../../components/card/CourseCard";
 import CardGroupWrapper from "../../wrappers/CardGroupWrapper";
 import CardGroupWithTitle from "../../wrappers/CardGroupWithTitle";
@@ -13,8 +12,6 @@ const Main = () => {
 
   return (
     <Stack spacing={2} alignItems={{ xs: "center", md: "flex-start" }}>
-      {/* <WhiteTypography variant="h5">Dashboard</WhiteTypography>
-      <WhiteTypography>Hi Robin! Good Luck with your studies!</WhiteTypography> */}
       <PageHeader
         title="Dashboard"
         subtitle="Hi Robin! Good Luck with your studies!"
