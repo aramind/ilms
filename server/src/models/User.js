e864dd10eaed6f1e93945580a8a769c58de535c4
aramind <88814622+aramind@users.noreply.github.com>
2024-08-03 23:42:50 +0800

    feat: adding fields for User model

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -3,9 +3,11 @@ const Schema = mongoose.Schema;
 
 const dotenv = require("dotenv");
 const getRoles = require("../utils/getRoles");
+const getStatuses = require("../utils/getStatuses");
 dotenv.config();
 
 const validRoles = getRoles.keys;
+const validStatuses = getStatuses.keys;
 
 const UserSchema = new Schema({
   email: {
@@ -34,6 +36,30 @@ const UserSchema = new Schema({
     required: true,
     enum: { values: validRoles },
   },
+  accessLevel: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: { values: validStatuses },
+  },
+  tokens: {
+    refresh: { type: String },
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+UserSchema.pre("findOneAndUpdate", async function (next) {
+  this._update.updatedAt = new Date();
 });
 
 module.exports = mongoose("User", UserSchema);
