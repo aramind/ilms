d17e73cc0f159d26678aa139db5a4c766e406faa
aramind <88814622+aramind@users.noreply.github.com>
2024-08-23 23:08:21 +0800

    feat: adding get enrolled course controller

---
+++ server/src/controllers/user/getEnrolledCourses.js
@@ -0,0 +1,34 @@
+const User = require("../../models/User");
+const sendResponse = require("../../utils/sendResponse");
+
+const getEnrolledCourses = async (req, res) => {
+  try {
+    const credentials = req.credentials;
+
+    const enrolledCourses = await User.findById(credentials?._id).populate({
+      path: "enrolledCourses.course",
+    });
+
+    console.log(enrolledCourses);
+
+    if (!enrolledCourses) {
+      return sendResponse.failed(
+        res,
+        "No enrolled courses on records",
+        null,
+        404
+      );
+    }
+
+    return sendResponse.success(
+      res,
+      "Enrolled courses successfully retrieved",
+      enrolledCourses,
+      200
+    );
+  } catch (error) {
+    return sendResponse.failed(res, "Network error", error, 500);
+  }
+};
+
+module.exports = getEnrolledCourses;
