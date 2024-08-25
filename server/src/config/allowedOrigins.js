74628856bc0d7d5c95f13a18ef83479fccb36612
aramind <88814622+aramind@users.noreply.github.com>
2024-08-25 16:13:26 +0800

    feat: adding site in cors

--- server/src/config/allowedOrigins.js
+++ server/src/config/allowedOrigins.js
@@ -2,6 +2,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5000",
   "https://ilms-djk6.onrender.com",
+  "https://ilms-6l0w.onrender.com/",
+
   //   "https://quebee-admin.onrender.com",
 ];
 module.exports = allowedOrigins;
