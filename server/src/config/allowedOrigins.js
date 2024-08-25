a9cede8a5b62f4b58994b6bf89c43a97cfa6c915
aramind <88814622+aramind@users.noreply.github.com>
2024-08-25 16:13:52 +0800

    fix: fixing typo

--- server/src/config/allowedOrigins.js
+++ server/src/config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5000",
   "https://ilms-djk6.onrender.com",
-  "https://ilms-6l0w.onrender.com/",
+  "https://ilms-6l0w.onrender.com",
 
   //   "https://quebee-admin.onrender.com",
 ];
