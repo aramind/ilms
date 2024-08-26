f9702365aef04a5c0ca2433e87d1e7ca4bbe0bc7
aramind <88814622+aramind@users.noreply.github.com>
2024-08-26 15:07:50 +0800

    feat: adding updating course progress controller

--- server/src/controllers/user/userController.js
+++ server/src/controllers/user/userController.js
@@ -1,11 +1,13 @@
 const enrollCourse = require("./enrollCourse");
 const getEnrolledCourses = require("./getEnrolledCourses");
 const register = require("./register");
+const updateCourseProgress = require("./updateCourseProgress");
 
 const userController = {
   register,
   enrollCourse,
   getEnrolledCourses,
+  updateCourseProgress,
 };
 
 module.exports = userController;
