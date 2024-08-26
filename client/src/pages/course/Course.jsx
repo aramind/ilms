39349e80335201a0162ef5256e683ad9ef8dd0c1
aramind <88814622+aramind@users.noreply.github.com>
2024-08-26 13:48:39 +0800

    fix: removing unnecessary codes

--- client/src/pages/course/Course.jsx
+++ client/src/pages/course/Course.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import MainLayoutWrapper from "../../wrappers/MainLayoutWrapper";
 import { useParams } from "react-router-dom";
 import {
