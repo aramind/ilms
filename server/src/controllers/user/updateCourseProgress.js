8b17817ba25a0ade87ea588d366ae89a11291ab9
aramind <88814622+aramind@users.noreply.github.com>
2024-08-26 15:03:13 +0800

    refactor: changing the implementation of the controller that handle updating course

--- server/src/controllers/user/updateCourseProgress.js
+++ server/src/controllers/user/updateCourseProgress.js
@@ -10,6 +10,11 @@ const updateCourseProgress = async (req, res) => {
 
     const userId = req?.credentials?._id;
 
+    // Validate input
+    if (!courseId || !topicId || !taskId) {
+      return sendResponse.failed(res, "Incomplete information", null, 400);
+    }
+
     const user = await User.findOne({ _id: userId });
 
     if (!user) {
@@ -17,47 +22,50 @@ const updateCourseProgress = async (req, res) => {
     }
 
     // check if the course already exists in the enrolledCOurses
-
-    const result = await User.updateOne(
-      { _id: userId },
-      {
-        $set: {
-          // add course if not present
-          "enrolledCourses.$[enrolledCourse].progress.$[progress].completedTasks":
-            {
-              $cond: {
-                if: {
-                  $in: [taskId, "enrolledCourse.progress.completedTopicTasks"],
-                },
-                then: {
-                  $pull: {
-                    "enrolledCourses.$[enrolledCourse].progress.$[progress].completedTopicTasks":
-                      taskId,
-                  },
-                },
-                else: {
-                  $addToSet: {
-                    "enrolledCourses.$[enrolledCourse].progress.$[progress].completedTopicTasks":
-                      taskId,
-                  },
-                },
-              },
-            },
-        },
-      },
-      {
-        arrayFilters: [
-          { "enrolledCourse.course": courseId },
-          { "progress.topic": topicId },
-        ],
-      }
+    const courseIndex = user.enrolledCourses.findIndex((ec) =>
+      ec.course.equals(courseId)
     );
+    if (courseIndex === -1) {
+      user.enrolledCourses.push({
+        course: courseId,
+        status: constants?.ENROLLED_COURSE_STATUSES?.[1],
+        progress: [{ topic: topicId, completedTopicTasks: [taskId] }],
+      });
+    } else {
+      const progressIndex = user.enrolledCourses[
+        courseIndex
+      ].progress.findIndex((p) => p.topic.equals(topicId));
+      if (progressIndex === -1) {
+        user.enrolledCourses[courseIndex].progress.push({
+          topic: topicId,
+          completedTopicTasks: [taskId],
+        });
+      } else {
+        const taskIndex =
+          user.enrolledCourses[courseIndex].progress[
+            progressIndex
+          ].completedTopicTasks.indexOf(taskId);
 
-    if (result.matchedCount === 0) {
-      return sendResponse.failed(res, "Record not found", null, 404);
+        if (taskIndex === -1) {
+          user.enrolledCourses[courseIndex].progress[
+            progressIndex
+          ].completedTopicTasks.push(taskId);
+        } else {
+          user.enrolledCourses[courseIndex].progress[
+            progressIndex
+          ].completedTopicTasks.splice(taskIndex, 1);
+        }
+      }
     }
 
-    return sendResponse.success(res, "Task updated successfully", result, 200);
+    await user.save();
+
+    return sendResponse.success(
+      res,
+      "Task updated successfully",
+      user?.enrolledCourses,
+      200
+    );
   } catch (error) {
     console.error(error);
     return sendResponse.failed(res, "Error updating task", error, 500);
