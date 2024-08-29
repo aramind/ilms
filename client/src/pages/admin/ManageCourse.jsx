b89b126b04b312371a5ff09e20c4e7662cdb8b2b
aramind <88814622+aramind@users.noreply.github.com>
2024-08-29 15:44:32 +0800

    feat: getting the created course by the user and passing it to the manage course page

--- client/src/pages/admin/ManageCourse.jsx
+++ client/src/pages/admin/ManageCourse.jsx
@@ -1,6 +1,11 @@
 import React from "react";
-
+import useCourseProvider from "../../hooks/useCourseProvider";
+import useAuth from "../../hooks/useAuth";
 const ManageCourse = () => {
+  const { coursesList } = useCourseProvider();
+  const { auth } = useAuth();
+
+  console.log(coursesList.filter((course) => course.creator === auth?._id));
   return <div>ManageCourse</div>;
 };
 
