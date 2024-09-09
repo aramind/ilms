6906792dc81187e80fb6f9eab0ab0d631155c098
aramind <88814622+aramind@users.noreply.github.com>
2024-09-09 22:57:30 +0800

    feat: selecting course from autoselector

---
+++ client/src/components/AutocompleteSelector.jsx
@@ -0,0 +1,29 @@
+import { Autocomplete, TextField } from "@mui/material";
+import React from "react";
+
+const AutocompleteSelector = ({ value, setValue, options, label = "" }) => {
+  return (
+    <Autocomplete
+      value={value}
+      onChange={(e, newValue) => setValue(newValue)}
+      options={options || {}}
+      getOptionLabel={(option) => {
+        if (option?.title || option?.acronym) {
+          return `${option?.title || ""} (${option?.acronym || ""})`;
+        } else {
+          return "";
+        }
+      }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          size="small"
+          variant="outlined"
+          placeholder="Type here to select.."
+        />
+      )}
+    />
+  );
+};
+
+export default AutocompleteSelector;
--- client/src/pages/admin/ManageCourse.jsx
+++ client/src/pages/admin/ManageCourse.jsx
@@ -2,23 +2,45 @@ import React, { useEffect, useState } from "react";
 import useCourseProvider from "../../hooks/useCourseProvider";
 import useAuth from "../../hooks/useAuth";
 import EditCourse from "./manage-course/EditCourse";
+import AutocompleteSelector from "../../components/AutocompleteSelector";
+import { Box, Button, Stack } from "@mui/material";
 const ManageCourse = () => {
   // const [selectedCourse, setSelectedCourse] = useState({});
   const { coursesList } = useCourseProvider();
   const { auth } = useAuth();
+  const [options, setOptions] = useState({});
   const [selectedCourse, setSelectedCourse] = useState({});
 
   useEffect(() => {
     // Only update if coursesList and auth are defined
     if (coursesList && auth?._id) {
-      const selected =
-        coursesList.find((course) => course.creator === auth._id) || {};
-      setSelectedCourse(selected);
+      // const selected =
+      //   coursesList.find((course) => course.creator === auth._id) || {};
+      // setSelectedCourse(selected);
+      setOptions((pv) => coursesList);
     }
-  }, [auth?._id, coursesList]);
+  }, [auth?._id, coursesList, setOptions]);
 
   return (
-    <>{selectedCourse && <EditCourse selectedCourse={selectedCourse} />}</>
+    <>
+      <Stack direction="row" width={1} mt={2} mb={3} alignItems="center">
+        <Box flex={1}>
+          <AutocompleteSelector
+            value={selectedCourse}
+            setValue={setSelectedCourse}
+            options={options || {}}
+            label="Select Course"
+          />
+        </Box>
+        <Stack flex={1} gap={2} direction="row" justifyContent="flex-end">
+          <Button variant="contained">temp button</Button>
+          <Button variant="contained">temp button</Button>
+          <Button variant="contained">temp button</Button>
+        </Stack>
+      </Stack>
+
+      {selectedCourse && <EditCourse selectedCourse={selectedCourse} />}
+    </>
   );
 };
 
