e1735635f0717cb7be25d31b617eca60e79ef27c
aramind <88814622+aramind@users.noreply.github.com>
2024-09-12 00:05:41 +0800

    refactor: reimplemeting addcourse and edit course components

--- client/src/pages/admin/add-course/AddCourse.jsx
+++ client/src/pages/admin/add-course/AddCourse.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useCourseReq from "../../../hooks/api/authenticated/useCourseReq";
 import useApiSend from "../../../hooks/api/useApiSend";
-import CourseForm from "../course-form/CourseForm";
-import { Button, Stack } from "@mui/material";
+import { useForm } from "react-hook-form";
+import WhiteTypography from "../../../components/WhiteTypography";
+import useConfirmActionDialog from "../../../hooks/useConfirmActionDialog";
 import FormWrapper from "../../../wrappers/FormWrapper";
+import { Box, Button, Stack } from "@mui/material";
+import MetaInfoSection from "./MetaInfoSection";
+import TopicsSection from "./TopicsSection";
+import { DevTool } from "@hookform/devtools";
+import LoadingPage from "../../LoadingPage";
 
 const defaultValues = {
   code: "",
@@ -16,8 +22,6 @@ const defaultValues = {
       title: "",
 
       description: "",
-
-      topicTasks: [{ action: "read", instruction: "", link: "" }],
     },
   ],
 };
@@ -33,14 +37,88 @@ const AddCourse = () => {
     }
   );
 
+  const {
+    control,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onTouched",
+    defaultValues: defaultValues,
+  });
+
+  const formMethods = {
+    control,
+    handleSubmit,
+    errors,
+  };
+
+  const handleClear = () => {
+    reset();
+  };
+
+  useEffect(() => {
+    reset(defaultValues);
+  }, [reset]);
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    sendAddCourseReq({ data });
+  };
+
+  const handleFormSubmit = () => {
+    handleSubmit(onSubmit)();
+  };
+
+  const { handleOpen: handleConfirm, renderConfirmActionDialog } =
+    useConfirmActionDialog();
+
+  const handleConfirmClear = () => {
+    handleConfirm(
+      "Confirm Reset",
+      <WhiteTypography>
+        Are you sure you want to reset all fields?
+      </WhiteTypography>,
+      handleClear
+    );
+  };
+
+  const handleConfirmSubmit = () => {
+    handleConfirm(
+      "Confirm Submit",
+      <WhiteTypography>
+        Are you sure you want to submit the form?
+      </WhiteTypography>,
+      handleFormSubmit
+    );
+  };
   return (
     <>
-      <CourseForm
-        sendFormCallback={sendAddCourseReq}
-        isLoading={isLoading}
-        defaultValues={defaultValues}
-        action="add"
-      />
+      <FormWrapper formMethods={formMethods}>
+        <form noValidate>
+          <Stack gap={2} px={1}>
+            <MetaInfoSection />
+            <TopicsSection control={control} />
+          </Stack>
+          <Box height="32px"></Box>
+          <Stack direction="row" justifyContent="flex-end" px={1} spacing={1}>
+            <Button variant="outlined" onClick={handleConfirmClear}>
+              Clear
+            </Button>
+
+            <Button
+              variant="contained"
+              onClick={handleConfirmSubmit}
+              sx={{ px: 5 }}
+            >
+              Submit
+            </Button>
+          </Stack>
+        </form>
+        <DevTool control={control} />
+      </FormWrapper>
+      <LoadingPage open={isLoading} text="Loading..." />
+      {renderConfirmActionDialog()}
     </>
   );
 };
--- client/src/pages/admin/manage-course/EditCourse.jsx
+++ client/src/pages/admin/manage-course/EditCourse.jsx
@@ -1,15 +1,92 @@
-import React from "react";
-import CourseForm from "../course-form/CourseForm";
+import React, { useEffect } from "react";
+import FormWrapper from "../../../wrappers/FormWrapper";
+import { Box, Button, Stack } from "@mui/material";
+import MetaInfoSection from "../add-course/MetaInfoSection";
+import TopicsSection from "../add-course/TopicsSection";
+import { useForm } from "react-hook-form";
+import useConfirmActionDialog from "../../../hooks/useConfirmActionDialog";
+import WhiteTypography from "../../../components/WhiteTypography";
 
 const EditCourse = ({ selectedCourse }) => {
+  const {
+    control,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onTouched",
+    defaultValues: selectedCourse,
+  });
+
+  const formMethods = {
+    control,
+    handleSubmit,
+    errors,
+  };
+
+  useEffect(() => {
+    reset(selectedCourse);
+  }, [reset, selectedCourse]);
+
+  const handleClear = () => {
+    reset();
+  };
+
+  const onSubmit = async (data) => {
+    console.log(data);
+  };
+
+  const handleFormSubmit = () => {
+    handleSubmit(onSubmit)();
+  };
+
+  const { handleOpen: handleConfirm, renderConfirmActionDialog } =
+    useConfirmActionDialog();
+
+  const handleConfirmClear = () => {
+    handleConfirm(
+      "Confirm Reset",
+      <WhiteTypography>
+        Are you sure you want to undo all changes?
+      </WhiteTypography>,
+      handleClear
+    );
+  };
+
+  const handleConfirmSubmit = () => {
+    handleConfirm(
+      "Confirm Submit",
+      <WhiteTypography>Save changes?</WhiteTypography>,
+      handleFormSubmit
+    );
+  };
+
   return (
     <>
-      <CourseForm
-        // sendFormCallback={sendAddCourseReq}
-        // isLoading={isLoading}
-        defaultValues={selectedCourse}
-        action="edit"
-      />
+      <FormWrapper formMethods={formMethods}>
+        <form noValidate>
+          <Stack gap={2} px={1}>
+            <MetaInfoSection />
+            <TopicsSection control={control} />
+          </Stack>
+          <Box height="32px"></Box>
+          <Stack direction="row" justifyContent="flex-end" px={1} spacing={1}>
+            <Button variant="outlined" onClick={handleConfirmClear}>
+              Undo Changes
+            </Button>
+
+            <Button
+              variant="contained"
+              onClick={handleConfirmSubmit}
+              sx={{ px: 5 }}
+            >
+              Save
+            </Button>
+          </Stack>
+        </form>
+      </FormWrapper>
+      {/* <LoadingPage open={isLoading} text="Loading..." /> */}
+      {renderConfirmActionDialog()}
     </>
   );
 };
