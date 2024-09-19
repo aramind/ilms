04bbd2d4b077d9fb8b4cbfe77fe1278e3c30d812
aramind <88814622+aramind@users.noreply.github.com>
2024-09-20 00:44:56 +0800

    fix: removing unused props

--- client/src/pages/admin/manage-students/UserInfo.jsx
+++ client/src/pages/admin/manage-students/UserInfo.jsx
@@ -1,4 +1,4 @@
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import React from "react";
 import ControlledLabelledTextField from "../../../components/controlled/ContLabelledTextField";
 import { Controller, useFormContext } from "react-hook-form";
@@ -15,26 +15,73 @@ const UserInfo = () => {
         <ControlledLabelledTextField label="last name" name="lastName" />
         <ControlledLabelledTextField label="email" name="email" />
       </Stack>
-      <Controller
-        control={control}
-        name="role"
-        render={({ field }) => (
-          <Stack flex={1}>
-            <LabelWrapper id="role" label="role" />
-            <ReusableSelect
-              labelId="role-select"
-              id="role-select"
-              value={field.value}
-              onChange={field.onChange}
-              styleProps={{ minWidth: "100px", maxWidth: "33%" }}
-              options={[
-                { label: "student", value: "student" },
-                { label: "admin", value: "admin" },
-              ]}
-            />
-          </Stack>
-        )}
-      />
+      <Stack direction="row" spacing={1}>
+        <Controller
+          control={control}
+          name="role"
+          render={({ field }) => (
+            <Stack flex={1}>
+              <LabelWrapper id="role" label="role" />
+              <ReusableSelect
+                labelId="role-select"
+                id="role-select"
+                value={field.value}
+                onChange={field.onChange}
+                styleProps={{ minWidth: "100px" }}
+                options={[
+                  { label: "student", value: "student" },
+                  { label: "admin", value: "admin" },
+                ]}
+              />
+            </Stack>
+          )}
+        />
+        <Controller
+          control={control}
+          name="status"
+          render={({ field }) => (
+            <Stack flex={1}>
+              <LabelWrapper id="status" label="status" />
+              <ReusableSelect
+                labelId="status-select"
+                id="status-select"
+                value={field.value}
+                onChange={field.onChange}
+                styleProps={{ minWidth: "100px" }}
+                options={[
+                  { label: "pending", value: "pending" },
+                  { label: "active", value: "active" },
+                  { label: "suspended", value: "suspended" },
+                  { label: "deleted", value: "deleted" },
+                ]}
+              />
+            </Stack>
+          )}
+        />
+        <Controller
+          control={control}
+          name="accessLevel"
+          render={({ field }) => (
+            <Stack flex={1}>
+              <LabelWrapper id="accessLevel" label="accessLevel" />
+              <ReusableSelect
+                labelId="accessLevel-select"
+                id="accessLevel-select"
+                value={field.value}
+                onChange={field.onChange}
+                styleProps={{ minWidth: "100px" }}
+                options={[
+                  { label: "1", value: "1" },
+                  { label: "2", value: "2" },
+                  { label: "3", value: "3" },
+                  { label: "4", value: "4" },
+                ]}
+              />
+            </Stack>
+          )}
+        />
+      </Stack>
+      <Typography>Enrolled Courses</Typography>
 
       {/* <ControlledLabelledTextField label="email" name="email" /> */}
     </Stack>
