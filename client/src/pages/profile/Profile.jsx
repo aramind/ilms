3f56b4e7c498551f63f173d180449aef907e2660
aramind <88814622+aramind@users.noreply.github.com>
2024-09-28 00:08:21 +0800

    clean: removing unneeded console logs

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,15 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import MainLayoutWrapper from "../../wrappers/MainLayoutWrapper";
 import useAuth from "../../hooks/useAuth";
-import useApiGet from "../../hooks/api/useApiGet";
-import useUserReq from "../../hooks/api/authenticated/useUserReq";
 import { useForm } from "react-hook-form";
-import ErrorPage from "../ErrorPage";
-import LoadingPage from "../LoadingPage";
 import FormWrapper from "../../wrappers/FormWrapper";
 import { Box, DialogActions, Stack, Typography } from "@mui/material";
 import ControlledLabelledTextField from "../../components/controlled/ContLabelledTextField";
-import WhiteTypography from "../../components/WhiteTypography";
 import DialogActionButton from "../../components/DialogActionButton";
 
 const LabelValue = ({ label, value }) => (
@@ -42,9 +37,7 @@ const Profile = () => {
   const handleReset = () => {
     reset(auth);
   };
-  // console.log(auth);
 
-  console.log(auth);
   const { firstName, lastName, enrolledCourses, ...readOnlyUserInfos } =
     auth?.userInfo;
 
