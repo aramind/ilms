62ce4e23ca757418093dec85c3ec6933c90e5da9
aramind <88814622+aramind@users.noreply.github.com>
2024-10-02 23:17:01 +0800

    fix: fixing open props being undefined

--- client/src/components/WhiteTypography.jsx
+++ client/src/components/WhiteTypography.jsx
@@ -1,8 +1,9 @@
 import { Typography } from "@mui/material";
+import { grey } from "@mui/material/colors";
 import React from "react";
 
 const WhiteTypography = ({ ...props }) => {
-  return <Typography color={(theme) => theme.palette.white.dark} {...props} />;
+  return <Typography sx={{ color: grey[300] }} {...props} />;
 };
 
 export default WhiteTypography;
