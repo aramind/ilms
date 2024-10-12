d84d6a496b7770e2eff25efd0ea10b9533b07cbf
aramind <88814622+aramind@users.noreply.github.com>
2024-10-13 01:38:02 +0800

    feat: passing the selected course

--- client/src/pages/admin/manage-course/ListOfStudents.jsx
+++ client/src/pages/admin/manage-course/ListOfStudents.jsx
@@ -3,7 +3,8 @@ import React from "react";
 import AutocompleteSelector from "../../../components/AutocompleteSelector";
 
 const studentsStatusOptions = ["all", "enrolled", "pending", "suspended"];
-const ListOfStudents = () => {
+const ListOfStudents = ({ selectedCourse }) => {
+  // const {data: studentsData, isLoading}
   return (
     <Box>
       <Typography>LIST OF STUDENTS</Typography>
@@ -14,6 +15,7 @@ const ListOfStudents = () => {
           options={studentsStatusOptions}
           label="Select Course"
         />
+        <Typography>{selectedCourse?.title} students</Typography>
       </Box>
     </Box>
   );
