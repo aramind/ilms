17d48df8400ca0088086a56ac9311693a5447c72
aramind <88814622+aramind@users.noreply.github.com>
2024-10-24 23:21:32 +0800

    feat: adding a separate page for sponsors

---
+++ client/src/pages/sponsors/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import MainLayoutWrapper from "../../wrappers/MainLayoutWrapper";
+import { Stack } from "@mui/material";
+
+const Main = () => {
+  return (
+    <>
+      <MainLayoutWrapper>
+        <Stack></Stack>
+      </MainLayoutWrapper>
+    </>
+  );
+};
+
+export default Main;
